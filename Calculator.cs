142b7a45eadd417c2d44d335cdcea91b10756845
hsbn <habib.h@students.amikom.ac.id>
2020-03-13 11:28:22 +0700

    calculator-fix

--- Calculator.cs
+++ Calculator.cs
@@ -1,25 +1,89 @@
-class Calculator
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
 {
-	static void Main(string[]args)
+	class Calculator
 	{
-		int a = 10;
-		int b = 6;
+		static void Main(string[] args)
+		{
+			/*
+			int a = 10;
+			int b = 6;
 		
-		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
-		Console.WriteLine("Hasil Penambahan: {0} - {1} = {2}", a, b, Penambahan(a, b));
+			Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
+			Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
+			*/
+			
+			Console.Title = "Aplikasi Calculator";
+            Console.WriteLine("Pilih menu Calculator:\n");
+            Console.WriteLine("1. Penambahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian\n");
+            Console.Write("Input nomor menu [1..4]: ");
+            
+            int pil = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+			
+			if (pil > 0 && pil < 5)
+            {
+
+                Console.Write("Inputkan nilai a = ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Inputkan nilai b = ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                switch (pil)
+                {
+                    case 1:
+                        Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a,b));
+                        break;
+                    case 2:
+                        Console.WriteLine("Hasil Penambahan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
+                        break;
+                    case 3:
+                        Console.WriteLine("Hasil Penambahan: {0} * {1} = {2}", a, b, Perkalian(a,b));
+                        break;
+                    case 4:
+                        Console.WriteLine("Hasil Penambahan: {0} / {1} = {2}", a, b, Pembagian(a,b));
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Maaf, menu yang Anda pilih tidak tersedia");
+            }
+
+
+            Console.WriteLine();
+			
+			Console.WriteLine("\nTekan sembarang key untuk keluar");
+			Console.ReadKey();
+		}
+	
+		static int Penambahan(int a, int b)
+		{
+			return a+b;
+		}
+	
+		static int Pengurangan(int a, int b)
+		{
+			return a-b;
+		}
 		
-		Console.WriteLine("\n Tekan Sembarang key untuk keluar");
-		Console.ReadKey();
+		static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static float Pembagian(float a, float b)
+        {
+            return a / b;
+        }
 	
-	}
-	
-	static int Penambahan(int a,int b)
-	{
-		return a + b;
-	}
-	
-	static int Pengurangan(int a,int b)
-	{
-		return a - b;
 	}
 }
